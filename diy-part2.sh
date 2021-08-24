c6e6e1c69465437e5f1605c4b58e6fc598545b46
allegiant <allegiant.xu@hotmail.com>
2021-08-24 13:23:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,16 +13,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-
-# add luci-app-ssr-plus
-git clone --depth=1 https://github.com/fw876/helloworld package/lean/helloworld
-
 # add luci-app-adblock-plus
 git clone --depth=1 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 
 ## overclock 1000Mhz--0x312 1100Mhz--0x362 1200Mhz--0x3B2
-#sed -i '57s/89/98/' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
-#sed -i 'N;118 a +\t\tif ((pll & 0x7f0) == 0x2b0) {\n+\t\t\tvolatile u32 i;\n+\n+\t\t\tpr_info("CPU Clock: 880MHz, start overclocking\\n");\n+\t\t\tpll &= ~0x7ff;\n+\t\t\tpll |= 0x362;\n+\t\t\trt_memc_w32(pll, MEMC_REG_CPU_PLL);\n+\t\t\tfor (i = 0; i < 1000; i++);\n+\t\t}' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+sed -i '57s/89/98/' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+sed -i 'N;118 a +\t\tif ((pll & 0x7f0) == 0x2b0) {\n+\t\t\tvolatile u32 i;\n+\n+\t\t\tpr_info("CPU Clock: 880MHz, start overclocking\\n");\n+\t\t\tpll &= ~0x7ff;\n+\t\t\tpll |= 0x362;\n+\t\t\trt_memc_w32(pll, MEMC_REG_CPU_PLL);\n+\t\t\tfor (i = 0; i < 1000; i++);\n+\t\t}' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 
