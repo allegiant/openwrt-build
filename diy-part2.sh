39789bf61bf857ec2c31ab0ea8bc3e6d7fa0f0e4
allegiant <allegiant.xu@hotmail.com>
2021-10-16 19:49:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # add luci-app-adblock-plus
 # git clone --depth=1 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
